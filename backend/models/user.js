91a2a07471fbafacb38427ca914a7b3ab67d79b5
Suriya528 <e.suriya528@gmail.com>
2026-08-16 23:27:57 +0530

    fix: Mongoose 9 pre-save hook - remove next() callback, use async/await

--- backend/models/user.js
+++ backend/models/user.js
@@ -48,15 +48,10 @@ StudentSchema.methods.calculateDevScore = function (posts, totalLikes, totalComm
   return (posts * 10) + (totalLikes * 5) + (this.followers.length * 8) + (totalComments * 3)
 }
 
-StudentSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next()
-  try {
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-    next()
-  } catch (err) {
-    next(err)
-  }
+StudentSchema.pre('save', async function () {
+  if (!this.isModified('password')) return
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
 })
 
 StudentSchema.methods.matchPassword = async function (enteredPassword) {
